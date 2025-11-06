7f814d5fbc41282bb051cbcdef911ba57970fa01
Noshir Patel <noshirpatel86@gmail.com>
2025-11-06 19:21:25 +0530

    Integrated unsplash API

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,12 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const env = import.meta.env;
+
 function App() {
+  const UNSPLASH_KEY = env.VITE_UNSPLASH_ACCESS_KEY;
   const [search, setSearch] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log('Search submitted for:', search);
+
+    fetch(
+      `https://api.unsplash.com/search/random?query=${search}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Unsplash API response:', data);
+        setSearch(''); // Clear search input after submission
+      })
+      .catch((error) => {
+        console.error('Error fetching from Unsplash API:', error);
+      });
   };
 
   return (
