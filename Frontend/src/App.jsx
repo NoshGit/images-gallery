b62ce43f3d545fd933758f314eb78d333e2edb42
Noshir Patel <noshirpatel86@gmail.com>
2025-11-07 23:43:06 +0530

    implemented image card with delete functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,28 +3,48 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const env = import.meta.env;
 
 function App() {
-  const UNSPLASH_KEY = env.VITE_UNSPLASH_ACCESS_KEY;
+  // const UNSPLASH_KEY = env.VITE_UNSPLASH_ACCESS_KEY;
+  const SOURCESPLASH_KEY = env.VITE_SOURCESPLASH_ACCESS_KEY;
+
   const [search, setSearch] = useState('');
+  const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log('Search submitted for:', search);
 
-    fetch(
-      `https://api.unsplash.com/search/random?query=${search}&client_id=${UNSPLASH_KEY}`
-    )
-      .then((response) => response.json())
+    //TODO: remove source splash API implementation once unsplash is Active.
+
+    // fetch(
+    //   `https://api.unsplash.com/search/random?query=${search}&client_id=${UNSPLASH_KEY}`
+    // )
+    fetch(`https://api.sourcesplash.com/api/random?q=${search}`, {
+      headers: {
+        Authorization: `Bearer ${SOURCESPLASH_KEY}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => {
-        console.log('Unsplash API response:', data);
+        setImages([{ title: search, ...data }, ...images]);
         setSearch(''); // Clear search input after submission
       })
       .catch((error) => {
-        console.error('Error fetching from Unsplash API:', error);
+        console.error('Error fetching from API:', error);
       });
+  };
+
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => id !== image.id));
   };
 
   return (
@@ -35,6 +55,17 @@ function App() {
         setValue={setSearch}
         handleSubmit={handleSearchSubmit}
       />
+
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {!!images.length &&
+            images.map((image, i) => (
+              <Col key={i}>
+                <ImageCard image={image} deleteImage={handleDeleteImage} />
+              </Col>
+            ))}
+        </Row>
+      </Container>
     </div>
   );
 }
